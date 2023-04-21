1daa479e3d270b24992a32140b447ec95cb22ebe
Данил Хальпуков <danil@MacBook-Air-Danil.local>
2023-04-21 15:33:09 +0300

    In progress

--- src/pages/Phone/Phone.jsx
+++ src/pages/Phone/Phone.jsx
@@ -1,17 +1,19 @@
 import axios from "axios";
 import React from "react";
+import sha256 from "crypto-js/sha256";
 
 const Telephony = () => {
-  const apiKey = "ug8gw0jvvjtson18s8k99jbjed6snptb";
+  const vpbx_api_key = "ug8gw0jvvjtson18s8k99jbjed6snptb";
+  const vpbx_api_salt = "crwyja1jy42t9gjkz3yenyhzi5xvczrb";
+  const json = {};
+  const sign = sha256(vpbx_api_key + json + vpbx_api_salt).toString();
+  console.log("sign: ", sign);
+
   const apiEndpoint =
-    "https://cors-anywhere.herokuapp.com/https://app.mango-office.ru/vpbx/stats/v1/calls?from=2023-04-19&to=2023-04-19";
+    "https://cors-anywhere.herokuapp.com/https://app.mango-office.ru/vpbx/account/balance";
 
   axios
-    .get(apiEndpoint, {
-      headers: {
-        "X-API-KEY": apiKey,
-      },
-    })
+    .post(apiEndpoint, sign)
     .then((response) => {
       console.log(response.data);
     })
